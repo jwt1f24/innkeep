b2d61cb3433b4c8d2792523971ff04f6770ed595
Jun Wei <jwpro637@gmail.com>
2026-07-25 15:44:29 +0800

    Added routing for hotel CRUD operations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from app.auth_router import router
+from app.auth_router import router as auth_router
+from app.hotels_router import router as hotels_router
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(auth_router)
+app.include_router(hotels_router)
