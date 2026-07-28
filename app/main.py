d310df4fae054ccf6a80f64b515be2e4e3a0829a
Jun Wei <jwpro637@gmail.com>
2026-07-28 16:04:45 +0800

    Added CORS middleware connection to backend FastAPI

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth_router, hotels_router, room_types_router, rooms_router, booking_router, pricing_router, payment_router
 
 app = FastAPI()
-routers = (auth_router, hotels_router, room_types_router, rooms_router, booking_router, pricing_router, payment_router)
 
+# connect middleware to backend for allowed fastapi requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# register routers
+routers = (auth_router, hotels_router, room_types_router, rooms_router, booking_router, pricing_router, payment_router)
 for module in routers:
     app.include_router(module.router)
