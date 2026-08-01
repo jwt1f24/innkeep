9a377513ea9d44d1ef7459f877699ea873ba7458
Jun Wei <jwpro637@gmail.com>
2026-08-01 16:20:48 +0800

    Added footer component content

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './AuthContext'
 import Navbar from './components/Navbar'
 import ProtectedRoute from './components/ProtectedRoute'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import Login from './pages/Login'
@@ -16,15 +17,18 @@ export default function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <div className="min-h-screen bg-slate-900">
-                <Navbar/>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/rooms" element={<Rooms/>} />
-                    <Route path="/bookings" element={<ProtectedRoute><Bookings/></ProtectedRoute>} />
-                </Routes>
+                <div className="min-h-screen bg-slate-900 flex flex-col">
+                    <Navbar/>
+                    <div className="flex-1">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/rooms" element={<Rooms/>} />
+                            <Route path="/bookings" element={<ProtectedRoute><Bookings/></ProtectedRoute>} />
+                        </Routes>
+                    </div>
+                    <Footer/>
                 </div>
             </AuthProvider>
         </BrowserRouter>
