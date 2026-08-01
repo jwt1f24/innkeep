75acdd73b80fdb85f19cc17b752f57dbac4e3d06
Jun Wei <jwpro637@gmail.com>
2026-08-01 18:59:55 +0800

    Added footer visibility condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { AuthProvider } from './AuthContext'
 import Navbar from './components/Navbar'
 import ProtectedRoute from './components/ProtectedRoute'
@@ -13,23 +13,32 @@ function Bookings() {
   return <div className="p-6 text-white">Your bookings (protected)</div>
 }
 
+function AppContent() {
+    const location = useLocation()
+    const hideFooter = ["/login", "/register"].includes(location.pathname)
+
+    return (
+        <div className="min-h-screen bg-slate-900 flex flex-col">
+            <Navbar/>
+            <div className="flex-1">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/rooms" element={<Rooms/>} />
+                    <Route path="/bookings" element={<ProtectedRoute><Bookings/></ProtectedRoute>} />
+                </Routes>
+            </div>
+            {!hideFooter && <Footer/>}
+        </div>
+    )
+}
+
 export default function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <div className="min-h-screen bg-slate-900 flex flex-col">
-                    <Navbar/>
-                    <div className="flex-1">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/rooms" element={<Rooms/>} />
-                            <Route path="/bookings" element={<ProtectedRoute><Bookings/></ProtectedRoute>} />
-                        </Routes>
-                    </div>
-                    <Footer/>
-                </div>
+                <AppContent/>
             </AuthProvider>
         </BrowserRouter>
     )
